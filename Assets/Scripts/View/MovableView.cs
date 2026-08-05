bf53b98468aa80ad938b118402d7d154b64592c8
Tatsuya Mukai <hhth24340005@class.hht.ac.jp>
2026-08-05 20:26:13 +0900

    add subject box prefab

---
+++ Assets/Scripts/View/MovableView.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public abstract class MovableView :
+  MonoBehaviour,
+  IMovableView
+{
+  [SerializeField]
+  private float moveSeconds = 0.5f;
+
+  public async UniTask MoveTo(
+    int x,
+    int y,
+    int z,
+    MoveMode moveMode,
+    CancellationToken ct
+  )
+  {
+    var originalPos = transform.localPosition;
+    var goalPos = new Vector3(x, y, z);
+    try
+    {
+      for (float elapsed = 0f; elapsed < moveSeconds; elapsed += Time.deltaTime)
+      {
+        transform.localPosition = Vector3.Lerp(originalPos, goalPos, elapsed / moveSeconds);
+        await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken: ct);
+      }
+    }
+    finally
+    {
+      transform.localPosition = goalPos;
+    }
+  }
+}
---
+++ Assets/Scripts/View/SubjectBoxView.cs
@@ -0,0 +1,1 @@
+public class SubjectBoxView : MovableView { }
