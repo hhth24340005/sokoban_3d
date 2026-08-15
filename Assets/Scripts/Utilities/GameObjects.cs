7465af6a6042b39afa954229854261f5e739f2d7
Tatsuya Mukai <hhth24340005@class.hht.ac.jp>
2026-08-15 23:04:05 +0900

    improve GameObjects

--- Assets/Scripts/Utilities/GameObjects.cs
+++ Assets/Scripts/Utilities/GameObjects.cs
@@ -10,54 +10,135 @@ public readonly struct ScopedGameObject : IDisposable
     GameObject = gameObject;
   }
 
-  public void Dispose() => UnityEngine.Object.Destroy(GameObject);
+  public void Dispose()
+  {
+    if (GameObject == null)
+    {
+      return;
+    }
+    GameObject.SetActive(false);
+    UnityEngine.Object.Destroy(GameObject);
+  }
 
   public static implicit operator GameObject(ScopedGameObject scope) => scope.GameObject;
 }
 
 public static class GameObjects
 {
-  public static ScopedGameObject ChildOf(
-    this GameObject parent,
+  public static bool IsPrefab(this GameObject self) => !self.scene.IsValid();
+
+  public static GameObject With<C>(
+    this GameObject self,
+    out C component,
+    bool throwIfPrefab = true,
+    bool allowDuplication = false
+  )
+    where C : Component
+  {
+    if (throwIfPrefab && self.IsPrefab())
+    {
+      throw new InvalidOperationException(
+        $"Attaching component {typeof(C)} to prefab {self} is not allowed"
+      );
+    }
+    component = null;
+    if (!allowDuplication)
+    {
+      component = self.GetComponent<C>();
+    }
+    if (component == null)
+    {
+      component = self.AddComponent<C>();
+    }
+    return self;
+  }
+
+  public static ScopedGameObject With<C>(
+    this ScopedGameObject self,
+    out C component,
+    bool thorwIfPrefab = true,
+    bool allowDuplication = false
+  ) where C : Component =>
+    new(self.GameObject.With(out component, thorwIfPrefab, allowDuplication));
+
+  // ===
+
+  public static ScopedGameObject CreateChild(
+    this Transform parent,
     string name
   )
   {
     var generated = new GameObject(name);
-    generated.transform.SetParent(parent.transform, worldPositionStays: false);
-    return new ScopedGameObject(generated);
+    generated.transform.SetParent(parent, worldPositionStays: false);
+    return new(generated);
   }
 
-  public static ScopedGameObject ChildOf<C>(
+  public static ScopedGameObject CreateChild(
     this GameObject parent,
-    C prefab,
-    out C component
-  ) where C : Component
-  {
-    var generated = UnityEngine.Object.Instantiate(prefab, parent.transform, false);
-    component = generated;
-    return new ScopedGameObject(generated.gameObject);
-  }
+    string name
+  ) => parent.transform.CreateChild(name);
 
-  public static ScopedGameObject ChildOf(
-    this GameObject parent,
-    GameObject prefab
+  public static ScopedGameObject CreateChild(
+    this ScopedGameObject parent,
+    string name
+  ) => parent.GameObject.CreateChild(name);
+
+  // ===
+
+  public static ScopedGameObject CreateChild(
+    this Transform parent,
+    GameObject gameObject,
+    bool copyIfExisting = true
   )
   {
-    var generated = UnityEngine.Object.Instantiate(prefab, parent.transform, false);
-    return new ScopedGameObject(generated);
+    GameObject ret;
+    if (copyIfExisting || gameObject.IsPrefab())
+    {
+      ret = UnityEngine.Object.Instantiate(gameObject, parent, worldPositionStays: false);
+    }
+    else
+    {
+      ret = gameObject;
+      gameObject.transform.SetParent(parent, worldPositionStays: false);
+    }
+    return new(ret);
   }
 
-  public static GameObject With<C>(this GameObject self, out C component)
-    where C : Component
-  {
-    component = self.AddComponent<C>();
-    return self;
-  }
+  public static ScopedGameObject CreateChild(
+    this GameObject parent,
+    GameObject prefab,
+    bool copyIfExisting = true
+  ) => parent.transform.CreateChild(prefab, copyIfExisting);
 
-  public static ScopedGameObject With<C>(this ScopedGameObject self, out C component)
-    where C : Component
-  {
-    component = self.GameObject.AddComponent<C>();
-    return self;
-  }
+  public static ScopedGameObject CreateChild(
+    this ScopedGameObject parent,
+    GameObject prefab,
+    bool copyIfExisting = true
+  ) => parent.GameObject.CreateChild(prefab, copyIfExisting);
+
+  // ===
+
+  public static ScopedGameObject CreateChild<C>(
+    this Transform parent,
+    C gameObject,
+    out C component,
+    bool copyIfExisting = true
+  ) where C : Component =>
+    parent
+      .CreateChild(gameObject.gameObject, copyIfExisting)
+      .With(out component);
+
+  public static ScopedGameObject CreateChild<C>(
+    this GameObject parent,
+    C gameObject,
+    out C component,
+    bool copyIfExisting = true
+  ) where C : Component => parent.transform.CreateChild(gameObject, out component, copyIfExisting);
+
+  public static ScopedGameObject CreateChild<C>(
+    this ScopedGameObject parent,
+    C gameObject,
+    out C component,
+    bool copyIfExisting = true
+  ) where C : Component => parent.GameObject.CreateChild(gameObject, out component, copyIfExisting);
 }
